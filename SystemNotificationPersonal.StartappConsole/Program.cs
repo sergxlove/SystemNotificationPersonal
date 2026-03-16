1bc6589aad27ff3c3f4364b19d3167fd1d3d2a07
sergxlove <sergxlove@mail.ru>
2026-03-16 19:04:41 +0600

    add console version start app

---
+++ SystemNotificationPersonal.StartappConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SystemNotificationPersonal.Core.Models;
+using SystemNotificationPersonal.StartappConsole.Cases;
+
+namespace SystemNotificationPersonal.StartappConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            ExecuteCommandCore cmd = new ExecuteCommandCore();
+            AppSettingStartApp appSetting = new();
+            appSetting.ReadConfig();
+            if (appSetting.FirstStart)
+            {
+                appSetting.CreateConfig();
+            }
+            DataCore data = new DataCore(appSetting);
+            cmd.AddRange(ConsoleCases.UseConsoleCases());
+            string commandLine = string.Empty;
+            bool exit = false;
+            Console.WriteLine("Используя данное программное обеспечение, вы принимаете все условия лицензии. См LISENCE.txt");
+            while (!exit)
+            {
+                Console.Write($"user > ");
+                commandLine = Console.ReadLine()!;
+                if (commandLine == "exit") exit = true;
+                cmd.ExecuteCommand(commandLine, data, serviceProvider);
+            }
+        }
+    }
+}
