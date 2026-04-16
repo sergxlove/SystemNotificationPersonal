de887a82eb12af13e9f42fcc040db1902176ec6a
sergxlove <sergxlove@mail.ru>
2026-04-16 15:47:40 +0600

    add app config server web

---
+++ SystemNotificationPersonal.ConfigServerWeb/Endpoints/ConfigEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace SystemNotificationPersonal.ConfigServerWeb.Endpoints
+{
+    public static class ConfigEndpoints
+    {
+        public static IEndpointRouteBuilder MapConfigEndpoints(this IEndpointRouteBuilder app)
+        {
+
+            return app;
+        }
+    }
+}
